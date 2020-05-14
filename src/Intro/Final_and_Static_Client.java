ea54cbbc58595f0efd27cc608664e5e47efdf6c3
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-14 23:58:46 +0530

    c

--- src/Intro/Final_and_Static_Client.java
+++ src/Intro/Final_and_Static_Client.java
@@ -3,6 +3,6 @@ package Intro;
 public class Final_and_Static_Client {
     public static void main(String[] args) {
         FInal_and_Static_KeyWord fInal_and_static_keyWord=new FInal_and_Static_KeyWord();
-        fInal_and_static_keyWord.rollno=1;
+//        fInal_and_static_keyWord.rollno=1;
     }
 }
