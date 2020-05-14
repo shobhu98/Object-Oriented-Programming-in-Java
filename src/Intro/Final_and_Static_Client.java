659ce186032dac17cbbeedb326ba359d38c35d0d
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-14 23:58:14 +0530

    static and final keyword

---
+++ src/Intro/Final_and_Static_Client.java
@@ -0,0 +1,8 @@
+package Intro;
+
+public class Final_and_Static_Client {
+    public static void main(String[] args) {
+        FInal_and_Static_KeyWord fInal_and_static_keyWord=new FInal_and_Static_KeyWord();
+        fInal_and_static_keyWord.rollno=1;
+    }
+}
