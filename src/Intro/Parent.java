836278ec071271dabce675a06e55142f6b2a2b34
SHOBHIT TIWARI <46121289+shobhu98@users.noreply.github.com>
2020-05-16 17:34:32 +0530

    test

--- src/Intro/Parent.java
+++ src/Intro/Parent.java
@@ -1,22 +1,22 @@
-package Intro;
-
- public class Parent {
-    public Parent(){
-        System.out.println("Parent class");
-    }
-   final public void func(){
-        System.out.println("I am func");
-    }
-
-}
-class Child extends Parent{
-
-
-
-    public Child(){
-        System.out.println("Child class");
-    }
-     public void func(){
-        System.out.println("I am func");
-    }
-}
+//package Intro;
+//
+// public class Parent {
+//    public Parent(){
+//        System.out.println("Parent class");
+//    }
+//   final public void func(){
+//        System.out.println("I am func");
+//    }
+//
+//}
+//class Child extends Parent{
+//
+//
+//
+//    public Child(){
+//        System.out.println("Child class");
+//    }
+//     public void func(){
+//        System.out.println("I am func");
+//    }
+////}A
